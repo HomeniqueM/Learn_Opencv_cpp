a5ff7bfc0d9019ba10579f792bd76fc8a6464d04
Homenique Vieira <Homenique.t@gmail.com>
2022-07-02 20:16:53 -0300

    Configurado o cmake

--- makefile
+++
@@ -1,8 +0,0 @@
-bluid: 
-	g++ ./src/*.cpp -o opcv.o -I /usr/include/opencv4
-
-run: bluid
-		./opcv.o
-
-clean: 
-	  rm opcv.o
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include<iostream>
+
+void inputopencv(){
+    cv::imread;
+}
 
 
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    std::string path;
+
     return 0;
 }
